2d665a67dee38572dc239ca4138b00fa77c2a96f
Ben R F <benramsayfoster@gmail.com>
2019-05-29 09:50:18 +0100

    Initial commit

---
+++ home.php
@@ -0,0 +1,73 @@
+<html>
+	<head>
+		<title>Ben R F</title>
+		<link rel="stylesheet" href="styles.css">
+		<link href="https://fonts.googleapis.com/css?family=Fira+Sans|Roboto&display=swap" rel="stylesheet">
+		<script src="stuff.js"></script>
+		<script>
+			window.onscroll = scrollCheck;
+			window.onfocus = resume;
+			window.onblur = pause;
+		</script>
+	</head>
+	<body id="b" onload="start()">
+		<div id="header" class="header attached">
+			<a href="javascript:void(0)" onclick="sTo(0)"> <p class="title">Ben Ramsay Foster</p> </a>
+			<a href="javascript:void(0)" onclick="sTo(3)"> <p class="header c">Contact</p> </a>
+			<a href="javascript:void(0)" onclick="sTo(2)"> <p class="header p">Projects</p> </a>
+			<a href="javascript:void(0)" onclick="sTo(1)"> <p class="header a">About</p> </a>
+			<div id="scroll" class="scroll underTitle"></div>
+		</div>
+		<div id="main" class="typingLock">
+			<p id="typing" class="type"></p>
+			<p id="typing1" class="type"></p>
+			<p id="typing2" class="type"></p>
+			<p id="typing3" class="type"></p>
+		</div>
+		<div id="about" class="aboutLock">
+			<div class="about">
+				<p class="about">I want to start with a story from the Onion. Because really, shouldn’t every talk start with a story from the Onion? This is from earlier this year.
+The headline reads: “Nation Shudders At Large Block Of Uninterrupted Text.”
+“Unable to rest their eyes on a colorful photograph or boldface heading that could be easily skimmed and forgotten, Americans collectively recoiled Monday when confronted with a solid block of uninterrupted text.
+“Dumbfounded citizens from Maine to California gazed helplessly at the frightening chunk of print, unsure of what to do next.
+“Without an illustration, chart, or embedded YouTube video to ease them in, millions were frozen in place, terrified by the sight of one long, unbroken string of English words.
+” ‘It demands so much of my time and concentration,’ said Chicago resident Dale Huza, who was confronted by the confusing mound of words early Monday afternoon. ‘This large block of text, it expects me to figure everything out on my own, and I hate it.’ ”
+This is a sign of how rough the outlook sometimes seems for our culture of reading and writing.
+In fact, every generation fears the death of literacy at the hands of some new media technology. And yet I’m here to share some optimism. After long existence as a confirmed cynic who shared the general belief in our imminent cultural doom, I felt an unfamiliar sensation 15 years ago when the Internet came over the horizon: I found myself becoming excited and hopeful.
+When I looked at the Internet I saw a medium that involves a huge amount of reading. Sure, a lot of it is presented in a highly decorated or distracting form. But a lot of it is in large blocks of uninterrupted text, too!
+And there’s something even more significant: The Web isn’t just inspiring a lot of reading. it has also opened the opportunity for a stunning amount of new writing.
+When I was growing up in the 1960s and 70s, the chief fear on behalf of literary culture was that television was going to destroy it. What if we were becoming a nation of passive, glassy-eyed couch potatoes — mindless consumers of numbing video entertainment?</p>
+			</div>
+		</div>
+		<div id="projects" class="projectLock">
+			
+		</div>
+		<div id="contact" class="contactLock">
+			<div id="benM" class="benM"></div>
+			<div class="details">
+				<div class="apology">
+					<p class="apology">I will always try my best to respond to chats using the BenM app, however if I am unavailable and the chat request times out then any of the following ways of communication will be responded to as early as possible.</p>
+				</div>
+				<div class="detail">
+					<img class="detail" src="icons/phone.png" height="80" width="80">
+					<p class="detail">07507764316</p>
+				</div>
+				<div class="detail">
+					<img class="detail" src="icons/mail.png" height="80" width="80">
+					<p class="detail" style="color: #e75a4d !important;">benramsayfoster@gmail.com</p>
+				</div>
+				<div class="detail">
+					<img class="detail" src="icons/git.png" height="80" width="80">
+					<a href="https://github.com/BenRF"><p class="detail link">BenRF</p></a>
+				</div>
+				<div class="detail">
+					<img class="detail" src="icons/discord.png" height="80" width="80">
+					<p class="detail" style="color: #7289da !important;">B3athunter #9950</p>
+				</div>
+			</div>
+		</div>
+		<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+		<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+		<script src="benM.js"></script>
+	</body>
+</html>
---
+++ stuff.js
@@ -0,0 +1,177 @@
+//Aren't you a nosy one?
+
+function sTo(target) {
+	document.body.scrollTop = window.innerHeight * target;
+	console.log(window.innerHeight*target);
+	return false;
+}
+
+function scrollCheck() {
+	if (document.body.scrollTop > 0) {
+		document.getElementById("header").className = "header detached";
+	} else {
+		document.getElementById("header").className = "header attached";
+	}
+	var height = window.innerHeight;
+	var s = document.getElementById("scroll");
+	var scrolled = document.body.scrollTop + (0.5*height);
+	var step1 = height * 1;
+	var step2 = height * 2;
+	var step3 = height * 3;
+	if (scrolled < step1) {
+		s.className = "scroll underTitle";
+	} else if (scrolled >= step1 && scrolled < step2) {
+		s.className = "scroll underAbout";
+	} else if (scrolled >= step2 && scrolled < step3) {
+		s.className = "scroll underProjects";
+	} else if (scrolled >= step3) {
+		s.className = "scroll underContact";
+	}
+}
+
+window.addEventListener('wheel',snap);
+function snap(e) {
+	if (e.deltaY < 0) {
+		console.log("up");
+		window.scrollBy(0,-(window.innerHeight));
+	} else {
+		console.log("down");
+		window.scrollBy(0,window.innerHeight);
+	}
+	e.preventDefault();
+}
+
+function jump(target) {
+	document.getElementById(target).scrollIntoView();
+}
+
+
+var message;
+var typing;
+var position = 0;
+var target = "";
+function pause() {
+	console.log("pause");
+	clearInterval(message);
+}
+
+function resume() {
+	console.log("resume");
+	start();
+}
+
+function start() {
+	message = setInterval(update, 500);
+	var flush;
+	var count = 0;
+	
+	function update() {
+		clearInterval(message);
+		var script = [["Hey",3],["Hey, welcome to my site.",5],["This site is all about Ben.",5],["He programs stuff.",5],["Because of this he's learnt a few languages.",5],["Such as Python,",2.5],["Such as Php,",1.5],["Such as Css,",1.5],["Such as C++,",1.5],["Such as MySql,",1.5],["Such as Java,",1.5],["Such as Javascript,",1.5],["Such as Jsp,",1.5],["Such as React.",1.5],["And more to come.",8]];
+		target = script[position][0];
+		change(script[position][0]);
+		message = setInterval(update,(script[position][1])*1000);
+		if  (position == script.length-1) {
+			flush = setInterval(fade,4000);
+			position = 0;
+		} else {
+			position++;
+		}
+	}
+	
+	function fade() {
+		clearInterval(flush);
+		var items = ["typing","typing1","typing2","typing3"];
+		for (var i = 0; i < items.length; i++) {
+			document.getElementById(items[i]).className = "type hide";
+		}
+		flush = setInterval(reappear,500);
+	}
+	
+	function reappear() {
+		clearInterval(flush);
+		var items = ["typing","typing1","typing2","typing3"];
+		for (var i = 0; i < items.length; i++) {
+			document.getElementById(items[i]).innerHTML = "";
+			document.getElementById(items[i]).className = "type";
+		}
+	}
+	
+}
+
+function change(target) {
+	var current = document.getElementById("typing");
+	if (difference(target,current.innerHTML) === target || difference(target,current.innerHTML) === current.innerHTML) {
+		store();
+	} else {
+		typing = setInterval(remove, 50);
+	}
+	
+	function pause() {
+		clearInterval(typing);
+		typing = setInterval(remove, 50);
+	}
+	
+	function store() {
+		document.getElementById("typing").id = "typing1";
+		document.getElementById("typing1").id = "typing2";
+		document.getElementById("typing2").id = "typing3";
+		var four = document.getElementById("typing3");
+		four.parentNode.removeChild(four);
+		var first = document.createElement("p");
+		first.setAttribute("id","typing");
+		first.setAttribute("class","type");
+		document.getElementById("b").appendChild(first);
+		current = document.getElementById("typing");
+		typing = setInterval(pause, 600);
+	}
+	
+	function remove() {
+		if (trim(current.innerHTML,target).length === current.innerHTML.length) {
+			clearInterval(typing);
+			typing = setInterval(add, 75);
+		} else {
+			current.innerHTML = (current.innerHTML).substring(0,current.innerHTML.length-1);
+		}
+	}
+	
+	function add() {
+		if (difference(current.innerHTML,target).length < 1) {
+			clearInterval(typing);
+		} else {
+			current.innerHTML = current.innerHTML + difference(current.innerHTML,target).substring(0,1);
+		}
+	}
+
+	function trim(s1,s2) {
+		if (s1.length > s2.length) {
+			var target = s1.length;
+		} else {
+			var target = s2.length;
+		}
+		for (var i = 0; i < target; i++) {
+			if (s1.charAt(i) != s2.charAt(i)) {
+				break;
+			}
+		}
+		return s1.substring(0,i);
+	}
+	
+	function difference(s1,s2) {
+		if (s1.length > s2.length) {
+			var target = s1.length;
+		} else {
+			var target = s2.length;
+		}
+		for (var i = 0; i < target; i++) {
+			if (s1.charAt(i) !== s2.charAt(i)) {
+				break;
+			}
+		}
+		if (s1.length > s2.length) {
+			return s1.substring(i,s1.length);
+		} else {
+			return s2.substring(i,s2.length);
+		}
+	}
+}
