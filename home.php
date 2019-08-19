b5097c0b60aade21b2394de4cb81b561cff821a6
Ben R F <benramsayfoster@gmail.com>
2019-08-19 21:03:18 +0100

    Finished BenM section

--- home.php
+++ home.php
@@ -68,10 +68,49 @@
 				<div class="base"></div>
 				<div class="stand"></div>
 				<div class="screen">
-						<div class="innerScreen"></div>
+						<div class="innerScreen">
+							<div class="window">
+								<div class="windowHeader">
+									<div class="headerBar">benrf.co.uk</div>
+									<div class="headerButtons">
+										<div class="headerBack"><</div>
+										<div class="headerForward">></div>
+									</div>
+									<div class="windowTabs">
+										<div class="windowMini"></div>
+										<div class="windowClose"></div>
+									</div>
+								</div>
+								<div class="chatC">
+									<div class="msgC">
+										<div class="sentC">Hey</div>
+									</div>
+									<div class="msgC">
+										<div class="recievedC">Hi there, how can I help?</div>
+									</div>
+									<div class="msgC">
+										<div class="sentC">I just had a question about your previous work experience</div>
+									</div>
+								</div>
+								<div class="msgBarC">
+									<div class="msgInput"></div>
+								</div>
+							</div>
+							<div class="taskBar">
+								<div class="windowStart"></div>
+							</div>
+						</div>
 				</div>
 				<div class="phoneDrawing">
-					<div class="phoneScreen"></div>
+					<div class="phoneScreen">
+						<div class="drawnMsgP recievedP">Hey</div>
+						<div class="drawnMsgP sentP">Hi there, how can I help?</div>
+						<div class="drawnMsgP recievedP">I just had a question about your previous work experience</div>
+						<div class="msgBarP">
+							<div class="msgInput"></div>
+						</div>
+					</div>
+					<div class="phoneButton"></div>
 				</div>
 		</div>
 			</div>
